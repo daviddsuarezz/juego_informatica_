f84786cfcbfbb1ec8d61a2e148ff83aba78d2a55
Sebastian-8a <s.ochoa@udea.edu.co>
2023-11-17 21:31:03 -0500

    Mover enemigo

--- DesarrolloPrimerMomento/enemigo.cpp
+++ DesarrolloPrimerMomento/enemigo.cpp
@@ -25,43 +25,47 @@ Enemigo::Enemigo()
 
 void Enemigo::desplazamiento()
 {
-    QPointF newPos;
+    int X = 0, Y = 0;
+
     int i = rand() % 4;
     switch(i){
     case 0:
         if (y()>0)
-            newPos.setX(x()), newPos.setY(y()-30);
-        else newPos.setX(x()), newPos.setY(y()+30);
+            X = x() , Y = (y()- 30);
+        else X = x(), Y = (y()+30);
         break;
     case 1:
         if (pos().y() < scene()->height() - 30)
-            newPos.setX(x()), newPos.setY(y()+30);
-        else newPos.setX(x()), newPos.setY(y()-30);
+            X = x(), Y = (y()+30);
+        else X = x(), Y = (y()-30);
         break;
     case 2:
         if (x()>0)
-            newPos.setX(x()- 30), newPos.setX(y());
-        else newPos.setX(x()+ 30), newPos.setX(y());
+            X = (x()- 30), Y = y();
+        else X = (x()+ 30), Y = y();
         break;
     case 3:
         if (pos().x() < scene()->width() - 30)
-            newPos.setX(x()+ 30), newPos.setX(y());
-        else newPos.setX(x()- 30), newPos.setX(y());
+            X = (x()+ 30), Y = y();
+        else X = (x()- 30), Y = y();
         break;
     default:
         break;
 
     }
+    QPointF direccion(30, 0); // Dirección hacia la derecha (puedes ajustar según tu caso)
+    qreal velocidad = 30; // Ajusta la velocidad según tus necesidades
+    QPointF newPos = pos() + direccion * velocidad;
 
     QList<QGraphicsItem*> itemsEnColision = collidingItems(mapToScene(newPos));
 
     foreach (QGraphicsItem* item, itemsEnColision) {
-        if (dynamic_cast<QGraphicsRectItem*>(item)) {
+        if (dynamic_cast<Enemigo*>(item)) {
             // Hay un muro en la colisión, no te muevas
             return;
         }
     }
-    setPos(newPos);
+    setPos(X,Y);
 
 }
 
